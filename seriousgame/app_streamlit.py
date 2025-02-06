f4e1699e4bcf69d717d62790f59a976c07c3d899
MOHAMED AMINE BALOUNSI <68426412+amine123b@users.noreply.github.com>
2025-02-06 18:07:51 +0000

    llol

--- seriousgame/app_streamlit.py
+++ seriousgame/app_streamlit.py
@@ -21,7 +21,7 @@ from pymongo.server_api import ServerApi
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://ortaraki:<Omaromar-1213>@cluster0.6tzyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://ortaraki:Omaromar12@cluster0.6tzyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
