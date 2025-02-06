c091ae3a184db2fb8946949d54b677c6769b6b06
MOHAMED AMINE BALOUNSI <68426412+amine123b@users.noreply.github.com>
2025-02-06 17:22:08 +0000

    llol

--- seriousgame/app_streamlit.py
+++ seriousgame/app_streamlit.py
@@ -15,24 +15,27 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 # Connect to MongoDB Atlas using the URI
+collection = []
+db = []
 try:
     client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
     
     # Ping the MongoDB server to check the connection
     client.admin.command('ping')  # Ping command to check if connection is established
     print("Successfully connected to MongoDB Atlas!")
-except ConnectionError as e:
-    print("Failed to connect to MongoDB Atlas:", e)
 
     # Access the database and collection
-db = client["games_database"]  # Specify the database name
-collection = db["games"]  # Specify the collection name
+    db = client["games_database"]  # Specify the database name
+    collection = db["games"]  # Specify the collection name
+
     # Test: Print collection data (optional)
-print(f"Connected to database: {db.name}")
-print(f"Connected to collection: {collection.name}")
+    print(f"Connected to database: {db.name}")
+    print(f"Connected to collection: {collection.name}")
     
-# except Exception as e:
-#     print("An error occurred:", e)
+except ConnectionError as e:
+    print("Failed to connect to MongoDB Atlas:", e)
+except Exception as e:
+    print("An error occurred:", e)
 
 # Upload Folders
 UPLOAD_FOLDER_GUIDES = os.getenv("UPLOAD_FOLDER_GUIDES")
