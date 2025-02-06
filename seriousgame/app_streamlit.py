dc06e680b5379178569f8b5236715f1d6ac82d66
MOHAMED AMINE BALOUNSI <68426412+amine123b@users.noreply.github.com>
2025-02-06 17:44:57 +0000

    llol

--- seriousgame/app_streamlit.py
+++ seriousgame/app_streamlit.py
@@ -12,8 +12,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MongoDB Connection
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 db = client["games_database"]  # Explicitly specify the database name
 collection = db["games"]
 
